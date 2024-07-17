2268530a347f8bcf90e470ff7f727c5b7e286824
gideonn12 <gideoneeman@icloud.com>
2024-07-17 17:35:23 +0300

    main changed to work with file

--- main.cpp
+++ main.cpp
@@ -7,26 +7,46 @@
 #include "Unbounded_Buffer.h"
 #include <vector>
 #include <thread>
+#include <iostream>
+#include <string>
+
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
     vector <Bounded_Buffer*> bufferIn;
     vector <Producer*> producers;
     vector <Unbounded_Buffer*> bufferOut;
     vector <CoEditor*> coeditors;
     vector <thread> threads;
-
-    for(int i =0; i<6; i++)
+    int numProducers = 0;
+    char* file1 = argv[1];
+    FILE *file = fopen(file1, "r");
+    if(file == NULL)
     {
-        Bounded_Buffer *b = new Bounded_Buffer(10);
-        bufferIn.push_back(b);
+        perror("error opening file");
+        return 1;
     }
-    // PRODUCERS
-    for(int i =0; i<6; i++)
+    char line[256];
+    int producerNumber, numberOfProducts, queueSize;
+    while(fgets(line, sizeof(line), file)!=NULL)
     {
-        Producer *p = new Producer(i+1,5,bufferIn[i]);
-        producers.push_back(p);
+        // Check for "PRODUCER" line and parse it
+        if (sscanf(line, "PRODUCER %d", &producerNumber) == 1) {
+            // Successfully parsed a producer line
+            // Assuming the next line is the number of products
+            if (fgets(line, sizeof(line), file) != NULL && sscanf(line, "%d", &numberOfProducts) == 1) {
+            }
+            // Assuming the next line is the queue size
+            if (fgets(line, sizeof(line), file) != NULL && sscanf(line, "queue size = %d", &queueSize) == 1) {
+                Bounded_Buffer *b = new Bounded_Buffer(queueSize);
+                bufferIn.push_back(b);
+                Producer *p = new Producer(producerNumber, numberOfProducts, b);
+                producers.push_back(p);
+                numProducers++;
+            }
+        }
     }
+    fclose(file);
     // SPORT NEWS WEATHER BUFFERS
     Unbounded_Buffer *sports = new Unbounded_Buffer("sports");
     Unbounded_Buffer *news = new Unbounded_Buffer("news");
@@ -46,7 +66,7 @@ int main()
     coeditors.push_back(news_editor);
     coeditors.push_back(weather_editor);
     // DISPATCHER
-    Dispatcher *d = new Dispatcher(6,bufferIn,bufferOut);
+    Dispatcher *d = new Dispatcher(numProducers,bufferIn,bufferOut);
 
     // RUN ALL THREADS
     // producer run
